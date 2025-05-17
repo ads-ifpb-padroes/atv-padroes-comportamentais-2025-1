cee99335dd7a94b7541061498a43d872e433515c
victorivis <knightdragonred@gmail.com>
2025-05-17 09:37:18 -0300

    Ajuste atividade 2

--- src/main/java/br/edu/ifpb/ads/padroes/atv2/visitante/Imprimir.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv2/visitante/Imprimir.java
@@ -6,13 +6,13 @@ import br.edu.ifpb.ads.padroes.atv2.item.Servico;
 public class Imprimir implements Visitante {
     @Override
     public void visitar(ProdutoFisico item) {
-        double preco = item.getPreco()*1.1;
+        double preco = item.getPreco();
         System.out.printf("%.2f\n", preco);
     }
 
     @Override
     public void visitar(Servico item) {
-        double preco = item.getPreco()*1.15;
+        double preco = item.getPreco();
         System.out.printf("%.2f\n", preco);
     }
 }
